0292e5c7b0e5e66a0d3c38796478212a782237b5
user174568 <user174568@74-80-245-39.ic2net.net>
2020-06-26 11:06:28 -0700

    ui and other update

--- NoteApp_GVA_112_368_344/AddEditNotesViewController.swift
+++ NoteApp_GVA_112_368_344/AddEditNotesViewController.swift
@@ -13,13 +13,11 @@ class AddEditNotesViewController: UIViewController {
     
     var player:AVAudioPlayer!
     var noteData = NoteData()
-    var editNoteData: NoteData?
-    var noteTableData = NoteViewController()
+
     var categorydata = CategoryData()
-    var isEdit = false
     var latitude1 = 0.0
     var longitude1 = 0.0
-    var audiostring = ""
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
